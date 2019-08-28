f38e8e1ac4da97231b5b9b24e3e2fdea1cb7e5f0
zengzhiwei <zengzhiwei@onemt.com.cn>
2019-08-28 21:00:23 +0800

    * 学习

---
+++ Algorithmic/冒泡排序实现_20190828.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def BubleSort(self, list):
+        if list is None:
+            return None
+
+
+if __name__ == '__main__':
+    list = [2,3,4,5,6,7,8,9,1,0]
+    Solution().BubleSort(list)
