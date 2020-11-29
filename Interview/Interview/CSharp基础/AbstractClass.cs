e89acc0650cca223035cba9b094d77523e4c8bc2
aver58 <308164213@qq.com>
2020-11-29 21:01:20 +0800

    * 题目整理
    Signed-off-by: aver58 <308164213@qq.com>

---
+++ Interview/Interview/CSharp基础/AbstractClass.cs
@@ -0,0 +1,21 @@
+﻿#region Copyright © 2020 Aver. All rights reserved.
+/*
+=====================================================
+ AverFrameWork v1.0
+ Filename:    AbstractClassDemo.cs
+ Author:      Zeng Zhiwei
+ Time:        2020\11\29 星期日 15:35:33
+=====================================================
+*/
+#endregion
+
+public abstract class AbstractClass
+{
+    public virtual void virtualMethod() { }
+    public abstract void method();
+}
+
+public abstract class AbstractChildClass: AbstractClass
+{
+    //public abstract void method();
+}
